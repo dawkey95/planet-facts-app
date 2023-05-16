41ca11a4d02d96d49c77cff6ba5d82f0d06e3afc
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-05-17 09:25:47 +1200

    init commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,84 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				black: 'var(--black)',
+				white: 'var(--white)',
+				darkGray: 'var(--dark-gray)',
+			},
+
+			fontFamily: {
+				sans: ['var(--font-inter)'],
+			},
+
+			fontSize: {
+				h1: [
+					'12.5rem',
+					{
+						lineHeight: '12.5rem',
+						letterSpacing: '-0.3125rem',
+						fontWeight: '700',
+					},
+				],
+
+				h2: [
+					'3.5rem',
+					{
+						lineHeight: '4.25rem',
+						fontWeight: '700',
+					},
+				],
+
+				h3: [
+					'1.5rem',
+					{
+						lineHeight: '1.75rem',
+						letterSpacing: '0.3rem',
+						fontWeight: '700',
+					},
+				],
+
+				h4: [
+					'1.25rem',
+					{
+						lineHeight: '1.75rem',
+						letterSpacing: '0.25rem',
+						fontWeight: '400',
+					},
+				],
+
+				h5: [
+					'1.125rem',
+					{
+						lineHeight: '1.75rem',
+						fontWeight: '700',
+					},
+				],
+
+				h6: [
+					'0.9375rem',
+					{
+						lineHeight: '1.75rem',
+						letterSpacing: '0.1875rem',
+						fontWeight: '400',
+					},
+				],
+
+				body: [
+					'1.125rem',
+					{
+						lineHeight: '1.75rem',
+						fontWeight: '400',
+					},
+				],
+			},
+		},
+	},
+	plugins: [],
+};
